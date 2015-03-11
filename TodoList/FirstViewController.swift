8fd3ec55fa2026d733210f7f40b245ce434312ec
TangTianlei <tangtianlei@Tianlei.local>
2015-03-10 20:25:59 -0700

    Initial Commit

---
+++ TodoList/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TodoList
+//
+//  Created by TangTianlei on 3/10/15.
+//  Copyright (c) 2015 TangTianlei. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
